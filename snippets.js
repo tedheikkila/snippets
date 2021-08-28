e5205f3014b2972da775d1271d298837fe386a56
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-28 18:04:50 -0500

    adding primitives and operators

--- snippets.js
+++ snippets.js
@@ -1,2 +1,45 @@
+// primitive data
 console.log("Yo")
+
+var studentName = "Ted"
+let studentAge = 100
+
+studentName = "Aragorn"
+studentAge = 300.5
+
+console.log(studentName + "_" + `is` + "_" + studentAge + "_" + `years old`)
+
+let thisIsABoolean = true
+thisIsABoolean = false
+
+console.log(thisIsABoolean)
+console.log(typeof(someUndefinedVariable))
+
+studentAge = false
+console.log(typeof(studentAge))
+
+// operators
+let a = 500
+let b = 50
+let c = "50"
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(b == c)
+console.log( b === c)
+
+let exp1 = (b == c)
+let exp2 = (a > b)
+
+console.log(exp1 && exp2)
+console.log(exp1 && !exp2)
+console.log(exp1 || !exp2)
+
+let exp3 = (a + b - c * a / b)
+console.log(exp3)
+
+
 
