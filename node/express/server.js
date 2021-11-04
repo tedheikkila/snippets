14d2c0ddbe388deda71a6238f723daea4284269c
Ted Heikkila <ted.heikkila@gmail.com>
2021-11-04 18:20:29 -0500

    setting add and view post pages. cont on trying to get all the posts to be viewed on the view page when the user clicks a view all posts btn

--- node/express/server.js
+++ node/express/server.js
@@ -8,6 +8,21 @@ const PORT = 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const posts = [
+  {
+    routeName: 'route1',
+    name: 'name1'
+  },
+  {
+    routeName: 'route2',
+    name: 'name2'
+  },
+  {
+    routeName: 'route3',
+    name: 'name3'
+  },
+];
+
 // serving up static html pages
 app.use(express.static('public'));
 
@@ -21,6 +36,16 @@ app.get('/routes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/routes.html'))
 );
 
+app.get('/add', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/add.html'))
+);
+
+app.get('/view', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/view.html'))
+);
+
+app.get('/api/posts', (req, res) => res.json(posts));
+
 // serving up json data
 app.get('/terms', (req, res) => res.json(termData));
 
@@ -29,6 +54,17 @@ app.get('*', (req, res) =>
     `<p>Bad request. Sorry try a different route</p>`
   )
 );
+
+// post
+app.post('/api/posts', (req, res) => {
+  const newPost = req.body;
+
+  newPost.routeName = newPost.name.replace(/\s+/g, '').toLowerCase()
+  console.log(newPost);
+
+  posts.push(newPost);
+  res.json(newPost);
+});
 
 // app listening
 app.listen(PORT, () =>
